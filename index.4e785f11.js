2983e81b76f075cfc0fa1a84477276c360f0fb52
Artem200921 <134134012+Artem200921@users.noreply.github.com>
2024-03-08 20:24:39 +0000

    Deploying to gh-pages from @ Artem200921/Bose_NC_700@4c80e070b4ded9630a309d1c3603e3accd61a0f2 🚀

---
+++ index.4e785f11.js
@@ -0,0 +1,2 @@
+var menuBtn=document.querySelector(".menu-btn"),menu=document.querySelector(".menu");menuBtn.addEventListener("click",(function(){menuBtn.classList.toggle("active"),menu.classList.toggle("active")}));
+//# sourceMappingURL=index.4e785f11.js.map
